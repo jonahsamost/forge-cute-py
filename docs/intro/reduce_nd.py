f56bba7c9ba555b2a27cd3a71d9214cab427b3b9
jonah <jsamost@gmail.com>
2026-02-04 06:05:31 -0800

    reduce over nd tensor works

--- docs/intro/reduce_nd.py
+++ docs/intro/reduce_nd.py
@@ -21,6 +21,7 @@ class ReduceSum:
         self.NEG_INF = Float32(float('-inf'))
         self.dim = dim
 
+        rank = len(self.shape)
         strides = [1]
         mult = 1
         for d in self.shape[1:][::-1]:
@@ -28,45 +29,56 @@ class ReduceSum:
             strides.insert(0, mult)
         self.strides = strides
 
-        self.before_shape = shape[:dim]
-        self.before_stride = strides[0] if dim > 0 else 0
-        self.before_prod = math.prod(self.before_shape) if dim > 0 else 1
-
-        self.after_shape = shape[dim + 1:]
-        self.after_stride = strides[-1] if dim < len(self.shape) - 1 else 0
-        self.after_prod = math.prod(self.after_shape) if dim < len(self.shape) - 1 else 1
-        
         self.reduce_size = shape[dim]
+
+        self.before_shape = shape[:dim]
+        self.after_shape = shape[dim + 1:]
+
+        self.before_prod = math.prod(self.before_shape) if dim > 0 else 1
+        self.after_prod = math.prod(self.after_shape) if dim < rank - 1 else 1
+
+        # self.before_stride = strides[dim - 1] if dim > 0 else 0
+        self.before_stride = strides[dim - 1] if dim > 0 else 0
+        self.after_stride = strides[-1] if dim < rank - 1 else 0
         self.reduce_stride = strides[dim]
+        
+        # self.reduce_stride = strides[dim]
 
         self.output_shape = (*self.before_shape, *self.after_shape)
         self.blocks = math.prod(self.output_shape)
 
     @cute.jit
     def __call__(self, gInput: cute.Tensor, gOutput: cute.Tensor, stream: cuda.CUstream = None):
-        blocks_over_reduce_dim = cute.ceil_div(self.reduce_size, self.warp_size)
 
+        reduce_cover_blocks = cute.ceil_div(self.reduce_size, self.warp_size)
         copy_op = cute.nvgpu.CopyUniversalOp()
         copy_atom = cute.make_copy_atom(copy_op, self.dtype, num_bits_per_copy=self.bits_read)
 
-        data_layout = cute.make_layout(
+        in_layout = cute.make_layout(
             (self.before_prod, self.reduce_size, self.after_prod),
             stride=(self.before_stride, self.reduce_stride, self.after_stride)
         ) 
-        gInputView = cute.make_tensor(gInput.iterator, data_layout)
+        gInputView = cute.make_tensor(gInput.iterator, in_layout)
+        out_layout = cute.make_layout(
+            (self.before_prod, self.after_prod),
+            stride=(self.after_prod, 1)
+        )
+        gOutputView = cute.make_tensor(gOutput.iterator, out_layout)
 
         thr_layout = cute.make_ordered_layout(
-            (self.warp_size, self.num_warps),
-            order=(0, 1)
+            (self.num_warps, self.warp_size),
+            order=(1, 0)
         )
         val_layout = cute.make_layout((1, 1))
         tiled_copy = cute.make_tiled_copy_tv(copy_atom, thr_layout, val_layout)
-        tiler_nd = (1, self.warp_size, self.num_warps)
-
-        print(f"input: {gInputView}")
+        tiler_nd = (
+            1,
+            reduce_cover_blocks * self.warp_size,
+            1
+        )
         blocks = cute.ceil_div(self.blocks, self.num_warps)
         self.kernel(
-            gInputView, gOutput, tiler_nd, tiled_copy
+            gInputView, gOutputView, tiler_nd, tiled_copy
         ).launch(
             grid=(blocks, 1, 1),
             block=(self.threads_per_block, 1, 1),
@@ -81,31 +93,26 @@ class ReduceSum:
         lane_idx = tidx % self.warp_size
 
         out_idx = bidx * self.num_warps + warp_idx
-        before_idx = out_idx // self.after_prod
-        after_idx = out_idx % self.after_prod
+        if out_idx < self.blocks:
+            before_idx = out_idx // self.after_prod
+            after_idx = out_idx % self.after_prod
 
-        gX = cute.local_tile(gInput, tiler_mn, (before_idx, None, after_idx))
-        tidxSlice = tiled_copy.get_slice(tidx)  
-        tidxIndices = tidxSlice.partition_S(gX)
-        if tidx == 0 and bidx == 0:
-            print(f"slice: {tidxSlice}")
-            print(f"ind: {tidxIndices}")
-        if bidx == 0 and tidx == 0:
-            print(f"tidx indices: {tidxIndices}")
-        tidxRegs = cute.make_rmem_tensor_like(tidxIndices)
-        cute.autovec_copy(tidxIndices, tidxRegs)
-        tidxValues = tidxRegs.load()
-        tidLocalSum = tidxValues.reduce(cute.ReductionOp.ADD, init_val=0.0, reduction_profile=0)
-        rowSum = cute.arch.warp_reduction_sum(tidLocalSum)
-        if lane_idx == 0:
-            gOutput[bidx * self.num_warps + warp_idx] = rowSum
+            gX = cute.local_tile(gInput, tiler_mn, (before_idx, 0, after_idx))
+            tidxSlice = tiled_copy.get_slice(tidx)  
+            tidxIndices = tidxSlice.partition_S(gX)
+            tidxRegs = cute.make_rmem_tensor_like(tidxIndices)
+            cute.autovec_copy(tidxIndices, tidxRegs)
+            tidxValues = tidxRegs.load()
+            tidLocalSum = tidxValues.reduce(cute.ReductionOp.ADD, init_val=0.0, reduction_profile=0)
+            rowSum = cute.arch.warp_reduction_sum(tidLocalSum)
+            if lane_idx == 0:
+                gOutput[before_idx, after_idx] = rowSum
 
 
-def benchmark(dim=1):
-    print(f"Testing dim: {dim}")
+def benchmark(shape, dim=1, do_bencharmk=False):
+    print(f"Testing dim: {dim}, {shape}")
     import time
 
-    a, b, c = 64, 128, 256
     dtype = torch.float32
     dtype_map = {
         torch.float16: Float16,
@@ -114,9 +121,7 @@ def benchmark(dim=1):
     }
     cute_dtype = dtype_map[dtype]
     
-    x = torch.randn(a, b, c, device='cuda', dtype=dtype)
-    print(f"x shape: {x.shape}")
-    print(f"reduce over dim: {dim}")
+    x = torch.randn(shape, device='cuda', dtype=dtype)
     output_shape = list(x.shape)
     output_shape.pop(dim)
     output = torch.empty(output_shape, device=x.device, dtype=x.dtype)
@@ -154,25 +159,31 @@ def benchmark(dim=1):
         max_diff = (output - expected).abs().max().item()
         print(f"         max diff: {max_diff}")
     
-    print("\nBenchmarks:")
-    
-    # Warmup
-    for _ in range(10):
-        fn(x, output)
-    torch.cuda.synchronize()
-    
-    # Benchmark our softmax
-    start = time.perf_counter()
-    for _ in range(100):
-        fn(x, output)
-    torch.cuda.synchronize()
-    print(f"  reduce sum cute dim=-1: {(time.perf_counter() - start) * 10:.3f} ms")
-    
-    # Compare to PyTorch
-    start = time.perf_counter()
-    for _ in range(100):
-        _ = torch.nn.functional.softmax(x, dim=-1)
-    torch.cuda.synchronize()
-    print(f"  torch reduce sum dim=-1:  {(time.perf_counter() - start) * 10:.3f} ms")
+    if do_bencharmk:
+        print("\nBenchmarks:")
+        
+        # Warmup
+        for _ in range(10):
+            fn(x, output)
+        torch.cuda.synchronize()
+        
+        # Benchmark our softmax
+        start = time.perf_counter()
+        for _ in range(100):
+            fn(x, output)
+        torch.cuda.synchronize()
+        print(f"  reduce sum cute dim=-1: {(time.perf_counter() - start) * 10:.3f} ms")
+        
+        # Compare to PyTorch
+        start = time.perf_counter()
+        for _ in range(100):
+            _ = torch.nn.functional.softmax(x, dim=-1)
+        torch.cuda.synchronize()
+        print(f"  torch reduce sum dim=-1:  {(time.perf_counter() - start) * 10:.3f} ms")
 
-benchmark(dim=1)
+
+shape = []
+for dim in [64, 32, 128, 256]:
+    shape.append(dim)
+    for i in range(len(shape)):
+        benchmark(shape, dim=i)
